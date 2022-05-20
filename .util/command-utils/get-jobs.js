284521843f4461ea8d25d6fcbbd9731710e9ccdb
Aaron Dalton <agdalton@live.com>
2022-05-20 15:20:00 -0500

    Update get-jobs.js

--- .util/command-utils/get-jobs.js
+++ .util/command-utils/get-jobs.js
@@ -5,11 +5,11 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = () => {
-    const files = fs.readdirSync(path.join(__dirname, '../ffxiv_jobs'))
+    const files = fs.readdirSync(path.join(__dirname, '../ffxiv-data/jobs'))
 	const jobs = []
 	for (const file of files) {
 		if (path.extname(file) === '.json') {
-			jobs.push(require(path.join(__dirname, '../ffxiv_jobs', file)))
+			jobs.push(require(path.join(__dirname, '../ffxiv-data/jobs', file)))
 		}
 	}
 

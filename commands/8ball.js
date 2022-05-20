21cdc9cef20ad89e72fd0a8f798c46c6f44a22b0
Aaron Dalton <agdalton@live.com>
2022-05-20 16:29:31 -0500

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -64,10 +64,8 @@ module.exports = {
 						.setDescription(
 							'What kind of intervention are you seeking?'
 						)
-						.addChoices([
-							{ name: 'Good', value: 'good' },
-							{ name: 'Bad', value: 'bad' },
-						])
+						.addChoices('Good', 'good')
+						.addChoices('Bad', 'bad')
 						.setRequired(true)
 				)
 		)

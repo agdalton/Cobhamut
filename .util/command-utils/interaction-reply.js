72eb76afaf130fca0c4b936bb223392d93879ccb
Aaron Dalton <agdalton@live.com>
2022-05-20 17:34:27 -0500

    Update interaction-reply.js

--- .util/command-utils/interaction-reply.js
+++ .util/command-utils/interaction-reply.js
@@ -9,6 +9,13 @@ module.exports = async (
 	ephemeral,
 	edit
 ) => {
+
+	await interaction.reply({
+		content: content,
+		embeds: embeds,
+		components: components,
+		ephemeral: ephemeral,
+	})
 	if (!edit) {
 		await interaction.reply({
 			content: content,

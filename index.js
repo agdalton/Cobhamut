cc7e3d05637c398ca82ab53ed5501c69ecd89969
Aaron Dalton <agdalton@live.com>
2022-05-20 17:45:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ client.on('ready', async () => {
 
 	readSelectMenus('./.util/select-menus')
 	// command DELETE
-	//await getApp().commands('977312385515782154').delete()
+	await getApp().commands('977336279651999784').delete()
 
 	// Log all commands
 	const globalCommands = await getApp().commands.get()

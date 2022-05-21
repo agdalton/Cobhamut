7e87c60187f4036f4dd4505d98fefa93c201ec02
Aaron Dalton <agdalton@live.com>
2022-05-21 13:25:36 -0500

    Update new.js

--- commands/subcommands/partyfinder/new.js
+++ commands/subcommands/partyfinder/new.js
@@ -45,10 +45,10 @@ module.exports = async (interaction, data, globals) => {
 		.setTitle('New Partyfinder')
 
 	const modalRow1 = new MessageActionRow().addComponents(description)
-	const modalRow3 = new MessageActionRow().addComponents(date)
-	const modalRow4 = new MessageActionRow().addComponents(time, timezone)
+	const modalRow2 = new MessageActionRow().addComponents(date)
+	const modalRow3 = new MessageActionRow().addComponents(time, timezone)
 
-	modal.addComponents(modalRow1, modalRow2, modalRow3, modalRow4)
+	modal.addComponents(modalRow1, modalRow2, modalRow3)
 
 	// Send the modal
 	await interaction.showModal(modal)

1adef156e599b0b73c77b28a60a05f476d28ade0
Aaron Dalton <agdalton@live.com>
2022-05-22 13:31:09 -0500

    Update partyfinder-modal.js

--- .util/modals/partyfinder-modal.js
+++ .util/modals/partyfinder-modal.js
@@ -31,33 +31,34 @@ module.exports = {
 		// --- Validate Party size, date, time, timezone --- //
 		const partyComp = getPartyComp(size)
 		const dataDTTZ = checkDTTZ(date, time, timezone)
-		// Setup err embed
-		const errEmbed = new MessageEmbed()
-			.setTitle('An error ocurred')
-			.setDescription(
-				'Cobhamut encountered an error while processing your Partyfinder command. See below for details.'
-			)
-			.setColor(error_red)
-			.setAuthor({
-				name: clientUsername,
-				iconURL: `${baseImageURL}/avatars/${clientID}/${clientAvatar}.png`,
-			})
-			.setThumbnail(
-				'https://cdn1.iconfinder.com/data/icons/basic-ui-elements-color-round/3/61-512.png'
-			)
-		// If invalid Party size
-		if (partyComp.hasOwnProperty('err')) {
-			const compErr = partyComp.err
-			errEmbed.addField(compErr.field, compErr.message)
-		}
-		// If date, time, timezone submitted is invalid
-		if (!dataDTTZ.isValid) {
-			// Add the errors to the embed
-			for (let iErr = 0; iErr < dataDTTZ.err.length; iErr++) {
-				errEmbed.addField(
-					dataDTTZ.err[iErr].field,
-					dataDTTZ.err[iErr].message
+		if (partyComp.hasOwnProperty('err') || !dataDTTZ.isValid) {
+			const errEmbed = new MessageEmbed()
+				.setTitle('An error ocurred')
+				.setDescription(
+					'Cobhamut encountered an error while processing your Partyfinder command. See below for details.'
 				)
+				.setColor(error_red)
+				.setAuthor({
+					name: clientUsername,
+					iconURL: `${baseImageURL}/avatars/${clientID}/${clientAvatar}.png`,
+				})
+				.setThumbnail(
+					'https://cdn1.iconfinder.com/data/icons/basic-ui-elements-color-round/3/61-512.png'
+				)
+			// If invalid Party size
+			if (partyComp.hasOwnProperty('err')) {
+				const compErr = partyComp.err
+				errEmbed.addField(compErr.field, compErr.message)
+			}
+			// If date, time, timezone submitted is invalid
+			if (!dataDTTZ.isValid) {
+				// Add the errors to the embed
+				for (let iErr = 0; iErr < dataDTTZ.err.length; iErr++) {
+					errEmbed.addField(
+						dataDTTZ.err[iErr].field,
+						dataDTTZ.err[iErr].message
+					)
+				}
 			}
 
 			interaction_reply(

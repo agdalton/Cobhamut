87d14a626d3d282599e1ebdcb7b84ff8a7c3743a
Aaron Dalton <agdalton@live.com>
2022-05-21 22:05:01 -0500

    Update modal-base.js

--- .util/modals/modal-base.js
+++ .util/modals/modal-base.js
@@ -7,6 +7,7 @@ module.exports = async (client, modal, globals) => {
 	// wait for an interaction
 	client.on('interactionCreate', (interaction) => {
 		// process command callback
+		console.log(interaction)
 		if (interaction.isModalSubmit() && name === interaction.customId)
 			callback(client, interaction, globals)
 

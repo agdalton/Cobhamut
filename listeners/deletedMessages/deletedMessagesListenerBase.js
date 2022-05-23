acbc51afc5e4f00a473d59f96d66bf81b8ad7040
Aaron Dalton <agdalton@live.com>
2022-05-23 17:45:16 -0500

    Update deletedMessagesListenerBase.js

--- listeners/deletedMessages/deletedMessagesListenerBase.js
+++ listeners/deletedMessages/deletedMessagesListenerBase.js
@@ -1,7 +1,7 @@
 // Message delete listener <-- executes when a message is deleted
 
-module.exports = async (client, reactionListener, globals) => {
-	let { callback } = reactionListener
+module.exports = async (client, deletedMessagesListener, globals) => {
+	let { callback } = deletedMessagesListener
 	client.on('messageDelete', async (message) => {
 		callback(message, globals)
 

117ee10f87b2b63030ec8d4d8ff0dafe764027e5
Aaron Dalton <agdalton@live.com>
2022-05-24 11:47:56 -0500

    Update updateMembers.js

--- listeners/reactions/partyfinder/updateMembers.js
+++ listeners/reactions/partyfinder/updateMembers.js
@@ -124,6 +124,7 @@ module.exports = {
 
 		// Fetch the full message
 		const message = await reaction.message.fetch()
+		console.log(message)
 		const updatedEmbed = createPFEmbed(
 			dataCreator,
 			globals,

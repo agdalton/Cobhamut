33cf974c39e91df00b929f73bf6bcd0c7586c8fd
Aaron Dalton <agdalton@live.com>
2022-06-05 14:49:44 -0500

    Update create.js

--- commands/subcommands/partyfinder/create.js
+++ commands/subcommands/partyfinder/create.js
@@ -13,7 +13,7 @@ module.exports = async (interaction, data, globals) => {
 	// Get subcommand optoins
 	const pfDescription = interaction.options.getString('description')
 	const pfSize = interaction.options.getString('size')
-	const pfPingRole = interaction.options.getRole('ping') || undefined
+	const pfPingRole = interaction.options.getRole('ping')
 	const pfDate = interaction.options.getString('date')
 	const pfTime = interaction.options.getString('time')
 	const pfAMPM = interaction.options.getString('ampm')
@@ -96,7 +96,7 @@ module.exports = async (interaction, data, globals) => {
 		guildID: interaction.guildId,
 		channelID: interaction.channelId,
 		originalResponseID: interactionResponse.id,
-		mentionRole: pfPingRole.toString(),
+		mentionRole: pfPingRole ? pfPingRole.toString() : null,
 		reminderSent: false,
 	}).save()
 

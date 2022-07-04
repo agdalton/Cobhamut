4f3ee0a084696212e565c1ffb5c0e28b752ca084
Aaron Dalton <agdalton@live.com>
2022-07-04 12:57:14 -0500

    Update rrCreateModal.js

--- .util/modals/raidReminder/rrCreateModal.js
+++ .util/modals/raidReminder/rrCreateModal.js
@@ -41,6 +41,27 @@ module.exports = {
 			return
 		}
 
+		// Validate the channel is a real channel
+		try {
+			// Fetch the guild the partyfinder was scheduled in
+			const guild = await client.guilds.fetch(guildID)
+			if (!guild) throw new Error() // Skip if the guild can't be fetched
+
+			// Fetch the channel of the guild the partyfinder was scheduled in
+			const channel = await guild.channels.fetch(channelID)
+			if (!channel) throw new Error() // Skip if the channel can't be fetched (i.e deleted)
+
+            if (channel.type !== 0) throw new Error() // If the selected channel is not a text channel, throw an error
+		} catch (e) {
+			inputs.err.push({
+				field: 'Channel',
+				message: "Cobhamut could not validate the channel you selected. Please ensure you're selecting a **text channel** from the pop-up list in Discord and that the selection is NOT a channel category (folder icon)",
+			})
+
+			sendInputError(interaction, memberData, globals, inputs)
+			return
+		}
+
 		// Get the date of the next reminder to be sent
 		const nextReminder = getNextReminder(inputs.days, time, timezone)
 

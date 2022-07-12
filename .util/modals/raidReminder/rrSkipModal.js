8ea0f428a0892dbde6326400ad5574b4197f0e1e
Aaron Dalton <agdalton@live.com>
2022-07-12 11:00:08 -0500

    Update rrSkipModal.js

--- .util/modals/raidReminder/rrSkipModal.js
+++ .util/modals/raidReminder/rrSkipModal.js
@@ -80,7 +80,10 @@ module.exports = {
 				reminderHours
 			)
 
-			if (daysOfWeek.length === 1) newNextReminder.plus({ days: 7 })
+			if (daysOfWeek.length === 1) {
+				console.log('Add 7!')
+				newNextReminder.plus({ days: 7 })
+			}
 
 			await raidReminderSchema.updateOne(
 				{

05b4344f99cb2f46ce9d9a90079c0d7293d6b38c
Aaron Dalton <agdalton@live.com>
2022-07-12 07:55:34 -0500

    Update create.js

--- commands/subcommands/raidreminder/create.js
+++ commands/subcommands/raidreminder/create.js
@@ -58,8 +58,8 @@ module.exports = async (interaction, data, globals) => {
 
 	// Add the rows to the modal
 	modal.addComponents(modalRow1, modalRow2, modalRow3, modalRow4, modalRow5)
-	modal.options = interaction.options
-	console.log(modal)
+	modal['options'] = interaction.options
+	console.log("MODAL OPTIONS\n\n" + modal.options)
 	// Send the modal
 	await interaction.showModal(modal)
 	return

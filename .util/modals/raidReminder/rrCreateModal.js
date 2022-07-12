e13b03d672bad0afc89c2224a1101ca55e7fa52f
Aaron Dalton <agdalton@live.com>
2022-07-12 08:59:50 -0500

    Update rrCreateModal.js

--- .util/modals/raidReminder/rrCreateModal.js
+++ .util/modals/raidReminder/rrCreateModal.js
@@ -54,7 +54,7 @@ module.exports = {
 		}
 
 		// Get modal inputs
-		console.log(interaction.message)
+		console.log(interaction)
 		const { fields } = interaction
 		const message = fields.getTextInputValue('rrMessage').trim()
 		const days = fields.getTextInputValue('rrDays').trim()

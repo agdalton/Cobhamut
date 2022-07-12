cbab366b1a577a7e896bc5c2de0d890f4b12c79b
Aaron Dalton <agdalton@live.com>
2022-07-12 10:24:16 -0500

    Update rrUpdateMessageModal.js

--- .util/modals/raidReminder/rrUpdateMessageModal.js
+++ .util/modals/raidReminder/rrUpdateMessageModal.js
@@ -113,7 +113,7 @@ module.exports = {
 			})
 
 		// Update the original message (the one with the select menu) so the menu disappears
-		await interaction.update({ embeds: [], components: [] })
+		await interaction.update({ embeds: [embed], components: [] })
 		// Follow up with the cancel confirmation
 		await interaction.followUp({ embeds: [embed], ephemeral: false })
 		return

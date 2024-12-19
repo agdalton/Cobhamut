08963650c79c339b6874e711d8823ab29563bc33
Aaron Dalton <agdalton@live.com>
2024-12-18 19:58:58 -0600

    .

--- listeners/reactions/reactionListenerBase.js
+++ listeners/reactions/reactionListenerBase.js
@@ -10,10 +10,7 @@ module.exports = async (client, reactionListener, globals) => {
 			console.log('There was an error fetching the message')
 		}
 
-		if (
-			applicableEmoji.includes(reaction._emoji.id) ||
-			applicableEmoji.includes(reaction._emoji.name)
-		)
+		if (applicableEmoji.includes(reaction._emoji.id) || applicableEmoji.includes(reaction._emoji.name))
 			callback(client, fullReaction, user, false, globals)
 		
 		console.log(JSON.stringify(applicableEmoji))
